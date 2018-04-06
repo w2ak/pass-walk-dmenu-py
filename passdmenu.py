e1d69efcf802e028c1852264b8058bc1b5b745a9
w2ak <w2ak@users.noreply.github.com>
2018-04-06 16:03:18 +0900

    Walk password file content
    Signed-off-by: w2ak <w2ak@users.noreply.github.com>

--- passdmenu.py
+++ passdmenu.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse as ap
-import logging, sys, time, os, glob, shutil, subprocess
+import yaml
+import logging, sys, time, os, glob, shutil, subprocess, functools
 lg = logging.getLogger(__name__)
 
 def failwith(rc, msg="Unspecified"):
@@ -52,15 +53,98 @@ def parse_args():
     logging.basicConfig(stream=a.log, level=lglvl)
     return a
 
+def merge_duplicates_constructor(loader, node, deep=False):
+    mapping = {}
+    for key_node, value_node in node.value:
+        key = loader.construct_object(key_node, deep=deep)
+        value = loader.construct_object(value_node, deep=deep)
+        if key in mapping:
+            if not isinstance(mapping[key],list):
+                mapping[key] = [ mapping[key] ]
+            mapping[key].append(value)
+        else:
+            mapping[key] = value
+    return mapping
+yaml.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+                    merge_duplicates_constructor)
+
+@functools.cmp_to_key
+def field_cmp(x, y):
+    if x == "password":
+        return -1
+    if y == "password":
+        return 1
+    if x == "username":
+        return -1
+    if y == "username":
+        return 1
+    if x < y:
+        return -1
+    if x > y:
+        return 1
+    return 0
+
+def getkeys_dict(d):
+    return list(d)
+
+def getchoices_dictkeys(d,k):
+    return sorted(list(map(str,k)), key=field_cmp)
+
+def getkeys_list(l):
+    return list(range(len(l)))
+
+def getchoices_listkeys(l,k):
+    return list(map(lambda x: '#{:d} ({:})'.format(x,str(l[x])[:10]),k))
+
+"""
+Get binary paths.
+"""
+bin_sed = which('sed')
+bin_pass = which('pass')
+bin_dmenu = which('dmenu')
+bin_xclip = which('xclip')
+bin_xdotool = which('xdotool')
+
+def dmenu_choice(choices, prefix=''):
+    cmd_dmenu = [ bin_dmenu, '-p', prefix ]
+    choice = getoutput(cmd_dmenu, input='\n'.join(choices))
+    try:
+        return choices.index(choice)
+    except ValueError:
+        return None
+
+def walk_object(o, prefixes=list(),getkeys=list,
+        getchoices=lambda y,x: list(map(str,x))):
+    keys = getkeys(o)
+    if len(keys)<1:
+        return o
+    if len(keys)==1:
+        key = keys[0]
+        prefixes.append(key)
+        return walk(o[key],prefixes=prefixes)
+    prefix = '.'.join(map(str,prefixes+['']))
+    choices = getchoices(o,keys)
+    index = dmenu_choice(choices, prefix=prefix)
+    return None if index is None else walk(o[keys[index]])
+
+def walk(o, prefixes=list()):
+    if isinstance(o,dict):
+        return walk_object(o, prefixes=prefixes,
+                getkeys=getkeys_dict, getchoices=getchoices_dictkeys)
+    if isinstance(o,list):
+        return walk_object(o, prefixes=prefixes,
+                getkeys=getkeys_list, getchoices=getchoices_listkeys)
+    return o
+
+def parse_pass_contents(contents):
+    sedscript = "1 s/^(.*)$/password: \"\\1\"/; 2,$ s/^(.*): ([^-].*)$/\\1: \"\\2\"/; /^otpauth/ s/^/otpauth: /;"
+    cmd_sed = [ bin_sed, '-r', sedscript ]
+    result = getoutput(cmd_sed, input=contents)
+    yobj = yaml.load(result)
+    return yobj
+
 if __name__=='__main__':
-    """
-    Get arguments and binary paths from PATH.
-    """
     args = parse_args()
-    bin_pass = which('pass')
-    bin_dmenu = which('dmenu')
-    bin_xclip = which('xclip')
-    bin_xdotool = which('xdotool')
 
     """
     Get the password store directory from the environment.
@@ -101,11 +185,14 @@ if __name__=='__main__':
     cmd_pass = [ bin_pass, entry ]
     content = getoutput(cmd_pass)
     lg.info("Password file contents stored.")
+    content = parse_pass_contents(content)
+    lg.info("Password file contents parsed.")
 
     """
     Get desired value.
     """
-    value = content.split('\n')[0].strip()
+    value = walk(content)
+    lg.info("Desired value obtained.")
 
     """
     Output the value.
