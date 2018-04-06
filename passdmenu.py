0361d4377288c2d0b59094ceafd7400cfbec1b15
w2ak <w2ak@users.noreply.github.com>
2018-04-06 11:54:19 +0900

    Password entry choice

--- passdmenu.py
+++ passdmenu.py
@@ -1,17 +1,44 @@
 #!/usr/bin/env python3
 import argparse as ap
-import logging, sys, time
+import logging, sys, time, os, glob, shutil, subprocess
 lg = logging.getLogger(__name__)
 
+def failwith(rc, msg="Unspecified"):
+    lg.error(msg)
+    sys.exit(rc)
+
+def which(prog):
+    binex = shutil.which(prog)
+    if binex is None:
+        failwith(1, msg="Could not find executable for '{:}'!".format(prog))
+    return binex
+
+def getoutput(args, input='', check=True):
+    result = subprocess.run(args,
+                            input=input.encode('ascii'),
+                            check=check,
+                            stdout=subprocess.PIPE)
+    output = result.stdout.decode('ascii')
+    if check:
+        return output
+    else:
+        return output, result.returncode
+
 def parse_args():
-    p = ap.ArgumentParser()
+    p = ap.ArgumentParser(description='Helps you get some password from your\
+            password-store. Then it either copies it to the clipboard\
+            or directly types it where your cursor is.')
     p.add_argument('-v', '--verbose', dest='verb',
                     action='count', default=0,
-                    help='Increase verbosity.')
+                    help='increase verbosity')
     p.add_argument('-l', '--log-file', dest='log',
                     type=ap.FileType('a'), default=sys.stderr,
-                    help='Log file.')
+                    metavar='<file>', help='log file')
+    p.add_argument('-t', '--type', dest='type',
+                    action='store_true', default=False,
+                    help='type obtained value somewhere')
     a = p.parse_args()
+
     lglvl = logging.DEBUG if a.verb > 2 else   \
             logging.INFO if a.verb > 1 else    \
             logging.WARNING if a.verb > 0 else \
@@ -23,10 +50,42 @@ def parse_args():
     return a
 
 if __name__=='__main__':
+    """
+    Get arguments and binary paths from PATH.
+    """
     args = parse_args()
-    lg.info("This is passdmenu version 0.")
-    lg.info("No function is available yet.")
-    lg.debug("This is a debug message. Yay.")
-    lg.warning("Please be careful when using this development version!")
-    lg.error("NotImplemented")
-    print(args)
+    bin_pass = which('pass')
+    bin_dmenu = which('dmenu')
+
+    """
+    Get the password store directory from the environment.
+    """
+    prefix = os.path.expanduser('~/.password-store')
+    prefix = os.getenv('PASSWORD_STORE_DIR', prefix)
+    ext = '.gpg'
+    lg.debug("Prefix:'{:}'".format(prefix))
+
+    """
+    Get the list of available passwords in the store.
+    """
+    iglob = os.path.join('**', '*' + ext)
+    iglob = os.path.join(prefix, iglob)
+    options = list()
+    for filename in glob.iglob(iglob, recursive=True):
+        passname = filename[1+len(prefix):-len(ext)]
+        lg.debug("Option found:'{:}'".format(passname))
+        options.append(passname)
+    lg.info("Found {:d} options in password store.".format(len(options)))
+
+    """
+    Get database entry name.
+    """
+    cmd_dmenu = [ bin_dmenu ]
+    entry,rc = getoutput(cmd_dmenu,
+                        input='\n'.join(options),
+                        check=False)
+    if rc:
+        failwith(rc,
+                msg = "Database entry choice failed with rc {:d}.".format(rc))
+    entry = entry.strip()
+    lg.info("Chosen database entry:'{:}'".format(entry))
